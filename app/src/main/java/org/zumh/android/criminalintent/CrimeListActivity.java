f4aa335b30f54b799722a68d28bbc0740b25c9b4
tizz98 <dev.tizz98@gmail.com>
2015-12-13 11:39:23 -0500

    list of crimes - end chp 9

--- app/src/main/java/org/zumh/android/criminalintent/CrimeListActivity.java
+++ app/src/main/java/org/zumh/android/criminalintent/CrimeListActivity.java
@@ -2,10 +2,10 @@ package org.zumh.android.criminalintent;
 
 import android.support.v4.app.Fragment;
 
-public class CrimeActivity extends SingleFragmentActivity {
+public class CrimeListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
