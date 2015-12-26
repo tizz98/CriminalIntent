44ba53d3c92b3b9afa607550e371d20f1de14bd4
tizz98 <dev.tizz98@gmail.com>
2015-12-26 10:45:58 -0500

    use ShareCompat.IntentBuilder

--- app/src/main/java/org/zumh/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/org/zumh/android/criminalintent/CrimeFragment.java
@@ -10,6 +10,7 @@ import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
@@ -187,12 +188,12 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
-                i.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.crime_report_subject));
-
-                i = Intent.createChooser(i, getString(R.string.send_report));
+                Intent i = ShareCompat.IntentBuilder.from(getActivity())
+                                        .setType("text/plain")
+                                        .setText(getCrimeReport())
+                                        .setSubject(getString(R.string.crime_report_subject))
+                                        .getIntent()
+                                        .setPackage("org.zumh.android.criminalintent");
                 startActivity(i);
             }
         });
